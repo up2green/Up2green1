9f514a81b74794f9d3d013a763ffa5e7cd375b75
Clement Gautier <clement.gautier.76@gmail.com>
2010-06-02 23:10:46 +0200

    ajout des controleur spécifiques aux frontend que l'on gère pour les url

--- web/moteur.php
+++ web/moteur.php
@@ -2,5 +2,5 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'prod', false);
+$configuration = ProjectConfiguration::getApplicationConfiguration('moteur', 'prod', true);
 sfContext::createInstance($configuration)->dispatch();

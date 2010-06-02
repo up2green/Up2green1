709d2aa693e7b3eda26083ea19fca8b2205d94f5
Clement Gautier <clement.gautier.76@gmail.com>
2010-06-02 22:44:52 +0200

    + bon path pour les images + modules backend pour les programmes et les organismes

---
+++ apps/backend/modules/organisme/lib/organismeGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * organisme module helper.
+ *
+ * @package    up2green
+ * @subpackage organisme
+ * @author     Clément Gautier
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class organismeGeneratorHelper extends BaseOrganismeGeneratorHelper
+{
+}

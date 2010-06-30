f56b4b2e2529944e13a03d692c1dae576f313b80
Clement Gautier <clement.gautier.76@gmail.com>
2010-06-30 23:58:56 +0200

    mise en place fonctionnel et refactoring du panel déroulant en bas.

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -16,60 +16,65 @@
                 <div class="logo"></div>
                 <div class="slogan">Etre acteur de la reforestation au fil de ses recherches</div>
             </div>
+            
             <?php echo $sf_content ?>
+            
             <div class="clear"></div>
-                <div class="pied">
-                    <div class="footer_ensavoirplus">
-                        <div class="footer_esp_left"></div>
-                        <div class="footer_esp_middle">en savoir plus</div>
-                        <div class="footer_esp_right"></div>
-                    </div>
-                    <div class="footer_top">
-                        <ul>
-                            <li><a href="#">Pourquoi planter des arbres</a></li>
-                            <li><a href="#">Nos programmes reforestation</a></li>
-                            <li><a href="#">Qui sommes-nous ?</a></li>
-                            <li><a href="#">Offrir des arbres en cadeau</a></li>
-                            <li><a href="#">Partenaires entreprises et collectivités</a></li>
-                        </ul>
-                    </div>
-                    <div class="footer_listes">
-                        <ul>
-                                                    <li><a href="#">Les chiffres clés de la déforestation</a></li>
-                                                    <li><a href="#">7 bonnes raisons de planter des arbres</a></li>
-                                                    <li><a href="#">La certification et les labels</a></li>
-                                                    <li><a href="#">Quelques idées reçues</a></li>
-                                                    <li><a href="#">Savez-vous planter un arbre ?</a></li>
-                                                    <li><a href="#">Vidéos sur la déforestation</a></li>
-                                                    </ul>
-                                                    <ul>
-                                                    <li><a href="#">Nos programmes de reforestation </a></li>
-                                                    <li><a href="#">Nos partenaires planteurs</a></li>
-                                                    <li><a href="#">Notre propre programme d’agroforesterie au Pérou</a></li>
-                                                    <li><a href="#">Les autres programmes que nous soutenons sur la Planète</a></li>
-                                                    <li><a href="#">Vidéos de reforestation</a></li>
-                                                    </ul>
-                                                    <ul>
-                                                    <li><a href="#">L’Association</a></li>
-                                                    <li><a href="#">Comment marche le moteur de recherche</a></li>
-                                                    <li><a href="#">L’équipe d’Up2green Reforestation</a></li>
-                                                    <li><a href="#">Up2green dans les Médias</a></li>
-                                                    </ul>
-                                                    <ul>
-                                                    <li><a href="#">Envoyez vos arbres via une e-card</a></li>
-                                                    <li><a href="#">Optez pour la carte cadeau reforestation</a></li>
-                                                    <li><a href="#">Le destinataire choisira lui-même les programmes où il souhaite planter ses arbres grâce à notre plate-forme de reforestation en ligne.</a></li>
-                                                    </ul>
-                                                    <ul>
-                                                    <li><a href="#">Une opportunité pour votre entreprise Impliquez vos clients et collaborateurs</a></li>
-                                                    <li><a href="#">Une opportunité pour votre collectivité Impliquez vos administrés</a></li>
-                                                    <li><a href="#">Nos références</a></li>
-                                                    <li><a href="#">Exemples de partenariats</a></li>
-                        </ul>
-                    </div>
-                    <div class="footer_bottom"></div>
-                </div>
+			<div id="footer">
+				<div class="onglet">
+					<div class="left"></div>
+					<div class="middle">en savoir plus</div>
+					<div class="right"></div>
+				</div>
+				<div class="head">
+					<ul>
+						<li><a href="#">Pourquoi planter des arbres</a></li>
+						<li><a href="#">Nos programmes reforestation</a></li>
+						<li><a href="#">Qui sommes-nous ?</a></li>
+						<li><a href="#">Offrir des arbres en cadeau</a></li>
+						<li><a href="#">Partenaires entreprises et collectivités</a></li>
+					</ul>
+				</div>
+				<div class="content_wrapper">
+					<div class="content_inner">
+						<ul>
+							<li><a href="#">Les chiffres clés de la déforestation</a></li>
+							<li><a href="#">7 bonnes raisons de planter des arbres</a></li>
+							<li><a href="#">La certification et les labels</a></li>
+							<li><a href="#">Quelques idées reçues</a></li>
+							<li><a href="#">Savez-vous planter un arbre ?</a></li>
+							<li><a href="#">Vidéos sur la déforestation</a></li>
+							</ul>
+							<ul>
+							<li><a href="#">Nos programmes de reforestation </a></li>
+							<li><a href="#">Nos partenaires planteurs</a></li>
+							<li><a href="#">Notre propre programme d’agroforesterie au Pérou</a></li>
+							<li><a href="#">Les autres programmes que nous soutenons sur la Planète</a></li>
+							<li><a href="#">Vidéos de reforestation</a></li>
+							</ul>
+							<ul>
+							<li><a href="#">L’Association</a></li>
+							<li><a href="#">Comment marche le moteur de recherche</a></li>
+							<li><a href="#">L’équipe d’Up2green Reforestation</a></li>
+							<li><a href="#">Up2green dans les Médias</a></li>
+							</ul>
+							<ul>
+							<li><a href="#">Envoyez vos arbres via une e-card</a></li>
+							<li><a href="#">Optez pour la carte cadeau reforestation</a></li>
+							<li><a href="#">Le destinataire choisira lui-même les programmes où il souhaite planter ses arbres grâce à notre plate-forme de reforestation en ligne.</a></li>
+							</ul>
+							<ul>
+							<li><a href="#">Une opportunité pour votre entreprise Impliquez vos clients et collaborateurs</a></li>
+							<li><a href="#">Une opportunité pour votre collectivité Impliquez vos administrés</a></li>
+							<li><a href="#">Nos références</a></li>
+							<li><a href="#">Exemples de partenariats</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="bottom"></div>
+			</div>
         </div>
+        
     </body>
 </html>
 

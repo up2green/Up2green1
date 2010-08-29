e3fa08045a4d3167fc314aa2bce751c72e7784a2
Clement Gautier <clement.gautier.76@gmail.com>
2010-08-29 18:30:26 +0200

    hot fix url plantation

--- apps/frontend/modules/blog/templates/_footer.php
+++ apps/frontend/modules/blog/templates/_footer.php
@@ -4,19 +4,19 @@
 			<ul class="footer_associatif">
 				<li class="first">
 					<p><?php echo __('footer_associatif_provide_trees'); ?></p>
-					<?php echo link_to(__('footer_associatif_gift_cards'), '@plantation', array('class' => 'button white')); ?>
+					<a class="button white" href="http://reforestation.up2green.fr" ><?php echo __('footer_associatif_gift_cards'); ?></a>
 				</li>
 				<li>
 					<p><?php echo __('footer_associatif_planting_trees'); ?></p>
-					<?php echo link_to(__('footer_associatif_search_engine'), '@plantation', array('class' => 'button white')); ?>
+					<a class="button white" href="http://reforestation.up2green.fr" ><?php echo __('footer_associatif_search_engine'); ?></a>
 				</li>
 				<li>
 					<p><?php echo __('footer_associatif_become_an_actor'); ?></p>
-					<?php echo link_to(__('footer_associatif_partnership'), '@plantation', array('class' => 'button white')); ?>
+					<a class="button white" href="http://reforestation.up2green.fr" ><?php echo __('footer_associatif_partnership'); ?></a>
 				</li>
 				<li>
 					<p><?php echo __('footer_associatif_have_pass'); ?></p>
-					<?php echo link_to(__('footer_associatif_use_my_pass'), '@plantation', array('class' => 'button white')); ?>
+					<a class="button white" href="http://reforestation.up2green.fr" ><?php echo __('footer_associatif_use_my_pass'); ?></a>
 				</li>
 			</ul>
 		</div>

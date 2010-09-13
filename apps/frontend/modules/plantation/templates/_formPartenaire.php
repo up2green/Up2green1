29c9d8543d1bc483ce8fa7ee339c8c4b3e639f0e
Clement Gautier <clement.gautier.76@gmail.com>
2010-09-13 22:06:32 +0200

    CSS purple, green, grey, blue2 revu pour les modules CSS purple pour les button

--- apps/frontend/modules/plantation/templates/_formPartenaire.php
+++ apps/frontend/modules/plantation/templates/_formPartenaire.php
@@ -1,10 +1,10 @@
-<div class="module">
-    <img class="title middle left" src="/images/module/purple/module_icon-partenaires_55x55.png" alt="" />
+<div class="module purple">
+    <img class="title middle left" src="/images/module/purple/icon/icon-partenaires.png" alt="" />
     <p class="title little indent"><?php echo $partenaire->getTitle() ?></p>
     <div class="content">
         <p><?php echo $partenaire->getAccroche() ?></p>
         <p class="center">
-            <a href="plantation/listeCouponsPartenaires" class="button rosy">Voir mes coupons</a>
+            <a href="plantation/listeCouponsPartenaires" class="button purple">Voir mes coupons</a>
         </p>
     </div>
     <?php include(sfConfig::get('sf_app_template_dir').'/module/border_and_corner.php') ?>

81ada6389459c7be92f1b4b56f2b3af9efb874fe
Clement Gautier <clement.gautier.76@gmail.com>
2010-10-03 20:16:59 +0200

    hotfix landing pages

--- apps/frontend/modules/landing/actions/actions.class.php
+++ apps/frontend/modules/landing/actions/actions.class.php
@@ -23,29 +23,32 @@ class landingActions extends sfActions
 	public function executePlantation(sfWebRequest $request)
 	{		
 		$partenaireOffset = $request->getParameter('partenaireOffset');
-		$user = Doctrine_Core::getTable('sfGuardUser')->findOneBy('username', $partenaireOffset);
 		$this->partenaire = null;
 		
-		if(!empty($user)) {
-			$this->partenaire = $user->getPartenaire();
-			$this->nbArbres = 0;
+		if(!empty($partenaireOffset)) {
+			$user = Doctrine_Core::getTable('sfGuardUser')->findOneBy('username', $partenaireOffset);
 			
-			if(!empty($this->partenaire)) {
-				// comptage des arbres planté par l'utilisateur partenaire directement
-				$this->nbArbres = Doctrine_Core::getTable('tree')
-					->createQuery('t')
-					->select('COUNT(id)')
-					->innerJoin('t.User tu')
-					->where('tu.user_id = ?', $user->getId())
-					->count();
-				// comptage des arbres planté par les coupons du partenaire
-				$this->nbArbres += Doctrine_Core::getTable('coupon')
-					->createQuery('c')
-					->select('COUNT(id)')
-					->innerJoin('c.Partenaire cp')
-					->where('cp.partenaire_id = ?', $this->partenaire->getId())
-					->andWhere('c.is_active = ?', 0)
-					->count();
+			if(!empty($user)) {
+				$this->partenaire = $user->getPartenaire();
+				$this->nbArbres = 0;
+				
+				if(!empty($this->partenaire)) {
+					// comptage des arbres planté par l'utilisateur partenaire directement
+					$this->nbArbres = Doctrine_Core::getTable('tree')
+						->createQuery('t')
+						->select('COUNT(id)')
+						->innerJoin('t.User tu')
+						->where('tu.user_id = ?', $user->getId())
+						->count();
+					// comptage des arbres planté par les coupons du partenaire
+					$this->nbArbres += Doctrine_Core::getTable('coupon')
+						->createQuery('c')
+						->select('COUNT(id)')
+						->innerJoin('c.Partenaire cp')
+						->where('cp.partenaire_id = ?', $this->partenaire->getId())
+						->andWhere('c.is_active = ?', 0)
+						->count();
+				}
 			}
 		}
 	}

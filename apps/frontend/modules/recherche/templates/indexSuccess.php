0d08ae8bec7884d1e9c7456732e549e2bf9f615d
Clement Gautier <clement.gautier.76@gmail.com>
2010-10-27 01:05:52 +0200

    refactoring css

--- apps/frontend/modules/recherche/templates/indexSuccess.php
+++ apps/frontend/modules/recherche/templates/indexSuccess.php
@@ -3,8 +3,8 @@
 		<div class="search">
 			<input type="hidden" id="hidden_text_search" name="hidden_text_search" value="<?php echo $textSearch ?>" />
 			<input type="hidden" id="hidden_moteur_search" name="hidden_moteur_search" value="<?php echo $moteur ?>" />
-			<div class="champs"><input type="text" id="recherche_text" name="recherche_text" size="65" value="<?php echo $textSearch ?>" /></div>
-			<div class="btn_search"><input type="submit" name="recherche_submit" value="Rechercher" /></div>
+			<input type="text" id="recherche_text" name="recherche_text" size="65" value="<?php echo $textSearch ?>" />
+			<input type="submit" id="recherche_submit" name="recherche_submit" value="Rechercher" class="button white small" />
 		</div>
 		<div class="more_search">
 			<div class="filtres">
@@ -42,51 +42,51 @@
 
 		</div>
 	</form>
-	<div class="menu_centre">
-		<?php if ($textSearch == ""): ?>
-		<div class="acteur">
-			<div class="module">
-		<img class="title middle left" src="/images/module/green/icon/acteur.png" alt="" />
-		<p class="title">Devenez acteur de la reforestation</p>
-		<div class="content">
-			<p>Créez votre compte et collectez GRATUITEMENT des arbres au fur et à mesure de vos recherches</p>
-			<p>Vous choisissez ensuite vous même où les planter sur la Planète parmi les programmes de reforestation que nous soutenons</p>
-			<p class="center">
-				<a href="#" class="button green">Créer un compte</a>
-			</p>
-			<p class="center">
-				<a href="#" class="button green">Définir Up2green comme moteur<br/>de recherche par defaut</a>
-			</p>
+	
+	<?php if ($textSearch == ""): ?>
+	<div id="bodyContentHomme">
+		
+		<div class="module acteur">
+			<img class="title middle left" src="/images/module/green/icon/acteur.png" alt="" />
+			<p class="title">Devenez acteur de la reforestation</p>
+			<div class="content">
+				<p>Créez votre compte et collectez GRATUITEMENT des arbres au fur et à mesure de vos recherches</p>
+				<p>Vous choisissez ensuite vous même où les planter sur la Planète parmi les programmes de reforestation que nous soutenons</p>
+				<p class="center">
+					<a href="#" class="button green">Créer un compte</a>
+				</p>
+				<p class="center">
+					<a href="#" class="button green">Définir Up2green comme moteur<br/>de recherche par defaut</a>
+				</p>
+			</div>
+			<?php include(sfConfig::get('sf_app_template_dir').'/module/border_and_corner.php') ?>
 		</div>
-		<?php include(sfConfig::get('sf_app_template_dir').'/module/border_and_corner.php') ?>
-	</div>
+		
+		<div class="module statistiques">
+			<img class="title middle right" src="/images/module/green/icon/stats.png" alt="" />
+			<p class="title">Statistiques</p>
+			<div class="content">
+				<p><img class="img_map" src="/images/moteur/stats_maps_200x70.png" /></p>
+				<p class="center">Arbres plantés : <a href="#">1353</a> <br/>Plus de <a href="#">4534</a> tonnes<br/> de CO</p>
+			</div>
+			<?php include(sfConfig::get('sf_app_template_dir').'/module/border_and_corner.php') ?>
 		</div>
-		<div class="statistiques">
-			<div class="module">
-		<img class="title middle right" src="/images/module/green/icon/stats.png" alt="" />
-		<p class="title">Statistiques</p>
-		<div class="content">
-		<p><img class="img_map" src="/images/moteur/stats_maps_200x70.png"</p>
-		<p class="center">Arbres plantés : <a href="#">1353</a> <br/>Plus de <a href="#">4534</a> tonnes<br/> de CO</p>
-		</div>
-		<?php include(sfConfig::get('sf_app_template_dir').'/module/border_and_corner.php') ?>
-	</div>
-
-			<div class="head_partenaires"><div class="titre_partenaires">Partenaires</div></div>
-			<div class="corps_partenaires">
-				<div class="contenus_partenaires">
-		Entreprises et collectivités, devenez acteur de la reforestation en impliquant vos administrés, client et colaborateur...
-		</div>
+		
+		<div class="module purple partenaires">
+			<img class="title middle right" src="/images/module/purple/icon/icon-partenaires.png" alt="" />
+			<p class="title">Partenaires</p>
+			<div class="content">
+				<p>Entreprises et collectivités, devenez acteur de la reforestation en impliquant vos administrés, client et colaborateur...</p>
 				<div class="lien_partenaires righter">
-		<a href="#">plus d'informations ici</a>
+					<a href="#">plus d'informations ici</a>
+				</div>
+			</div>
+			<?php include(sfConfig::get('sf_app_template_dir').'/module/border_and_corner.php') ?>
 		</div>
-
-			</div>
-			<div class="pied_partenaires">
-			</div>
+	
 	</div>
-	</div>
-		<?php else: ?>
+	<?php else: ?>
+	<div id="bodyContent">
 		<?php
 		if ($moteur == SearchEngine::WEB) {
 			foreach ($results as $result) { echo include_partial("web", array("result" => $result)); echo "<hr />" ;}
@@ -104,6 +104,6 @@
 		<div class="more-result">
 			<span id="searchMore" class="button white big">Plus de Résultats</span>
 		</div>
-		<?php endif ; ?>
 	</div>
+	<?php endif ; ?>
 </div>

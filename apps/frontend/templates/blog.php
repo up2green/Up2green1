bdeda22362aa7e638eef5a5b636ca042f234857e
Clement Gautier <clement.gautier.76@gmail.com>
2010-11-12 13:04:03 +0100

    Ajout de Google Analytics

--- apps/frontend/templates/blog.php
+++ apps/frontend/templates/blog.php
@@ -8,6 +8,22 @@
 		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
+
+	<script type="text/javascript">
+
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-19664821-1']);
+		_gaq.push(['_setDomainName', '.up2green.com']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+
+	</script>
+
   </head>
   <body>
     <?php /* include_partial('blog/menuSocial'); */ ?>

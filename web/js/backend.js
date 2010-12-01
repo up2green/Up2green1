d2f3ffb6e2dc542d682afb6e3b4c4de188007de1
Clement Gautier <clement.gautier.76@gmail.com>
2010-12-01 22:30:18 +0100

    Fix d'un bug JS sur e menu admin qui m'énervais depuis longtemps

--- web/js/backend.js
+++ web/js/backend.js
@@ -1,22 +1,14 @@
 $(document).ready(function(){
 
 	var menuWrapper = $("#menu-wrapper");
-
+	
 	//Only shows drop down trigger when js is enabled (Adds empty span tag after ul.subnav*)
 	$("ul.subnav", menuWrapper).parent().append("<span></span>"); 
 	
-	$("#menu li").mouseenter(function() { //When trigger is clicked...
-
-		$(this).find("ul.subnav").slideDown('fast').show();
-
-		$(this).hover(function() {}, function(){
-			$(this).find("ul.subnav").slideUp('slow');
-		});
-
-	}).hover(function() {
-		$(this).addClass("subhover");
-	}, function(){
-		$(this).removeClass("subhover");
+	$("#menu > li").hover(function(e) {
+		$(this).find("ul.subnav").stop(true, true).slideDown('fast');
+	}, function(e){
+		$(this).find("ul.subnav").slideUp('fast');
 	});
 
 });

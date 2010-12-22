a433b8c38148e3c3c6a0ef332b39256e498b09f6
Clement Gautier <clement.gautier.76@gmail.com>
2010-12-22 07:48:58 +0100

    Legal policy pour DDC et contact

--- apps/frontend/modules/blog/templates/_footerLegal.php
+++ apps/frontend/modules/blog/templates/_footerLegal.php
@@ -1,17 +1,35 @@
 <div id="footer_page">
   <div id="legal">
-    <span>Up2green® 2010 </span>
-    <?php foreach($category->getActiveLinks() as $link) : ?>
-	<?php
+
+    <?php 
+		$first = true;
+		foreach($category->getActiveLinks() as $link) :
+
 		$linkDisplay = $link->getSrc();
 		$target = '_self';
 
 		if(preg_match('/http/', $linkDisplay)) {
 			$target = '_blank';
 		}
+
+		echo '<span>';
+
+		if(!$first) {
+			echo ' | ';
+		}
 	?>
-  	<span>| <a target="<?php echo $target; ?>" href="<?php echo $linkDisplay; ?>"><?php echo $link->getTitle(); ?></a></span>
-  	<?php endforeach; ?>
+  	<a target="<?php echo $target; ?>" href="<?php echo $linkDisplay; ?>"><?php echo $link->getTitle(); ?></a></span>
+  	<?php 
+		$first = false;
+		endforeach;
+	?>
+	<br />
+	<p>
+		Association Up2green Reforestation<br />
+		38 rue Desaix<br />
+		75015 Paris - FRANCE<br />
+		Contact : <a href="mailto:contact@up2green.com">contact@up2green.com</a>
+	</p>
   </div>
   <div id="copyright">
     <p>

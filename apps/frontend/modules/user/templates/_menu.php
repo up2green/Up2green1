bc5bebc418eb8f75efcbd7a1c544f106292d4a53
Clement Gautier <clement.gautier.76@gmail.com>
2010-12-28 15:45:16 +0100

    Update symfony

--- apps/frontend/modules/user/templates/_menu.php
+++ apps/frontend/modules/user/templates/_menu.php
@@ -2,29 +2,25 @@
 	<div class="left_menu_haut"></div>
 	<div class="middle_menu_haut">
 	
-		<?php if(!$sf_user->isAuthenticated()):?>
-		
 		<div class="menu_accueil">
-			<div class="flag"></div>
+			<div class="flag">
+				<?php echo include_component('user', 'language'); ?>
+			</div>
 			<ul class="menu">
+				<?php if(!$sf_user->isAuthenticated()):?>
 				<li><a class="button small green" href="/"><?php echo __("Accueil") ?></a></li>
 				<li><a class="button small green" href="<?php echo url_for("user/inscription"); ?>"><?php echo __("Créer mon compte") ?></a></li>
-			</ul>
-		</div>
-		<div class="menu_connexion">
-			<?php echo include_component('user', 'sideSignin'); ?>
-		</div>
-		
-		<?php else: ?>
-		
-		<div class="menu_accueil">
-			<div class="flag"></div>
-			<ul class="menu">
+				<?php else: ?>
 				<li><a class="button small green" href="/"><?php echo __("Accueil") ?></a></li>
 				<li><a class="button small gray disabled" href="#"><?php echo __("Définir comme page d'accueil") ?></a></li>
+				<?php endif; ?>
 			</ul>
 		</div>
+
 		<div class="menu_connexion">
+			<?php if(!$sf_user->isAuthenticated()):?>
+			<?php echo include_component('user', 'sideSignin'); ?>
+			<?php else: ?>
 			<ul class="menu">
 				<li><a href="<?php echo url_for('@sf_guard_signout') ?>"><?php echo __("Déconnexion") ?></a></li>
 				<li><a href="<?php echo url_for('@plantation') ?>"><?php echo __(
@@ -36,9 +32,8 @@
 					array('{username}' => $sf_user->getUsername())
 				) ?></a></li>
 			</ul>
+			<?php endif; ?>
 		</div>
-		
-		<?php endif; ?>
 
 	</div>
 	<div class="right_menu_haut"></div>

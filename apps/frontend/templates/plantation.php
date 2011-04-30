dfa030d6e8662a19a2a9b28c7406f78af1ce14cd
Clement Gautier <clement.gautier.76@gmail.com>
2011-04-30 23:08:44 +0200

    OpenSearch + BugFix JS sur IE + BugFix title languagebar

--- apps/frontend/templates/plantation.php
+++ apps/frontend/templates/plantation.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
-        <?php include_http_metas() ?>
-        <?php include_metas() ?>
-        <?php include_title() ?>
-        <link rel="icon" href="favicon.ico" type="image/x-icon" />
-		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-        <?php include_stylesheets() ?>
-        <?php include_javascripts() ?>
+			<?php include_http_metas() ?>
+			<?php include_metas() ?>
+			<?php include_title() ?>
+			<link rel="icon" href="favicon.ico" type="image/x-icon" />
+			<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+			<link title="up2green Search" type="application/opensearchdescription+xml" rel="Search" href="opensearch.xml" />
+			<?php include_stylesheets() ?>
+			<?php include_javascripts() ?>
 
 		<script type="text/javascript">
 

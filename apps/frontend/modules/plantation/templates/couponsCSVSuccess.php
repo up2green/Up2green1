614793a484970435ff165f7e4b257e161f27cb07
Clement Gautier <clement.gautier.76@gmail.com>
2011-05-03 12:04:34 +0200

    BugFix #81 : export de coupons

--- apps/frontend/modules/plantation/templates/couponsCSVSuccess.php
+++ apps/frontend/modules/plantation/templates/couponsCSVSuccess.php
@@ -1,10 +1,11 @@
 <?php 
-header('Content-Type: application/csv;charset=utf-8;name=up2green-coupons-'.date('d-m-Y').'.csv');
+header('Content-Type: application/csv;charset=UTF-8;name=up2green-coupons-'.date('d-m-Y').'.csv');
 header('Content-Disposition: attachment;filename=up2green-coupons-'.date('d-m-Y').'.csv');
 
 $columns = array(
 	__("Code coupon"),
 	__("Crédit"),
+	__("Utilisé ?"),
 	__("Date d'utilisation"),
 );
 
@@ -12,5 +13,5 @@ echo '"'.implode('","', $columns).'"';
 ?>
 
 <?php foreach ($coupons as $coupon): ?>
-"<?php echo $coupons['code'] ?>","<?php echo $couponGens[$coupons['gen_id']] ?>","<?php echo $coupons['is_active'] ? '-' : $coupons['used_at'] ?>"
+"<?php echo $coupon['code'] ?>","<?php echo $couponGens[$coupon['gen_id']]['credit'] ?>","<?php echo $coupon['is_active'] ? __("Non") : __("Oui") ?>","<?php echo $coupon['is_active'] ? '-' : $coupon['used_at'] ?>"
 <?php endforeach; ?>

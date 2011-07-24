1d6caf30803eb13b2d1b7598501e77531f3e59ff
Clement Gautier <clement.gautier.76@gmail.com>
2011-07-24 22:02:15 +0000

    HotFix

--- apps/frontend/modules/partenaire/templates/_logos.php
+++ apps/frontend/modules/partenaire/templates/_logos.php
@@ -2,7 +2,7 @@
 	<?php foreach ($partenaire->getLogos() as $logo) : ?>
 	<li class="partenaire-logo">
 		<a href="<?php echo $logo->getHref() ?>">
-			<img class="organisme-image" src="<?php $logo->getPath() ?>" alt="Logo" />
+			<img class="organisme-image" src="<?php echo $logo->getPath() ?>" alt="Logo" />
 		</a>
 	</li>
 	<?php endforeach; ?>

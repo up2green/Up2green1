ac942ad6761619ee36fbcbd2c4a1751096b08d05
Clement Gautier <clement.gautier.76@gmail.com>
2012-02-22 00:33:30 +0100

    typo

--- lib/model/doctrine/programmeTable.class.php
+++ lib/model/doctrine/programmeTable.class.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Manage Queries for the programme Table 
+ */
 class programmeTable extends Doctrine_Table
 {
 
@@ -11,14 +14,15 @@ class programmeTable extends Doctrine_Table
    * @param <Integer> $offset Offset du programme de départ
    * @return <Query> Requête
    */
-  protected function buildQueryForRetrieveLastProgrammes($culture, $number = 2, $offset = 0) {
+  protected function buildQueryForRetrieveLastProgrammes($culture, $number = 2, $offset = 0)
+  {
     $q = $this->createQuery('p')
-			->leftJoin('p.Translation t')
-			->where('t.lang = ?', $culture)
-			->andWhere('p.is_active = ?', 1)
-			->orderBy('p.created_at DESC')
-			->limit($number)
-			->offset($offset);
+      ->leftJoin('p.Translation t')
+      ->where('t.lang = ?', $culture)
+      ->andWhere('p.is_active = ?', 1)
+      ->orderBy('p.created_at DESC')
+      ->limit($number)
+      ->offset($offset);
     return $q;
   }
 
@@ -30,7 +34,8 @@ class programmeTable extends Doctrine_Table
    * @param <Integer> $offset Offset du programme de départ
    * @return <List<Programme>> Liste des $number derniers programmes
    */
-  public function retrieveLastProgrammes($culture, $number = 2, $offset = 0) {
+  public function retrieveLastProgrammes($culture, $number = 2, $offset = 0)
+  {
     return $this->buildQueryForRetrieveLastProgrammes($culture, $number, $offset)->execute();
   }
 
@@ -42,204 +47,243 @@ class programmeTable extends Doctrine_Table
    * @param <Integer> $offset Offset du programme de départ
    * @return <array> Tableau des $number derniers programmes
    */
-  public function retrieveLastProgrammesInArray($culture, $number = 2, $offset = 0) {
+  public function retrieveLastProgrammesInArray($culture, $number = 2, $offset = 0)
+  {
     return $this->buildQueryForRetrieveLastProgrammes($culture, $number, $offset)->fetchArray();
   }
-  
-	public function countTrees($programmes = array()) {
 
-		$treeTable = Doctrine_Core::getTable('tree');
+  public function countTrees($programmes = array())
+  {
 
-		$q = $this->createQuery('p')
-			->select('p.id, COUNT(t.id) AS nbTree')
-			->innerJoin('p.Trees t');
+    $treeTable = Doctrine_Core::getTable('tree');
 
-		if(!empty($programmes)) {
-			$q = $q->whereIn('p.id', $programmes);
-		}
+    $q = $this->createQuery('p')
+      ->select('p.id, COUNT(t.id) AS nbTree')
+      ->innerJoin('p.Trees t');
 
-		$q = $q->groupBy('p.id');
-		$ret = $this->getArray($q);
+    if (!empty($programmes)) {
+      $q = $q->whereIn('p.id', $programmes);
+    }
 
-		foreach($ret as &$programme) {
-			if(isset($treeTable::$hardcode[(int)$programme['id']])) {
-				$programme['nbTree'] += $treeTable::$hardcode[$programme['id']];
-			}
-		}
+    $q   = $q->groupBy('p.id');
+    $ret = $this->getArray($q);
 
-		return $ret;
-	}
-  
-	public function getArrayById(Doctrine_Query $q = null) {
-		$results = $this->getArray($q);
-		$ret = array();
-		foreach($results as $result) {
-			$ret[$result['id']] = $result;
-		}
-		return $ret;
-	}
+    foreach ($ret as &$programme) {
+      if (isset($treeTable::$hardcode[(int) $programme['id']])) {
+        $programme['nbTree'] += $treeTable::$hardcode[$programme['id']];
+      }
+    }
 
-	// -----------------------------------------
-	// DRY
-	// -----------------------------------------
-		
-	public function getArrayActiveBySlug($slug, Doctrine_Query $q = null) {
-		return $this->getArrayActive($this->addSlugQuery($slug, $q));
-	}
-  
-	public function countActiveBySlug($slug, Doctrine_Query $q = null) {
-		return $this->countActive($this->addSlugQuery($slug, $q));
-	}
+    return $ret;
+  }
 
-	public function getOneActiveBySlug($slug, Doctrine_Query $q = null) {
-		return $this->getOneActive($this->addSlugQuery($slug, $q));
-	}
+  public function getArrayById(Doctrine_Query $q = null)
+  {
+    $results = $this->getArray($q);
+    $ret     = array();
+    foreach ($results as $result) {
+      $ret[$result['id']] = $result;
+    }
+    return $ret;
+  }
 
-	public function getActiveBySlug($slug, Doctrine_Query $q = null) {
-		return $this->getActive($this->addSlugQuery($slug, $q));
-	}
-	
-	public function getArrayBySlug($slug, Doctrine_Query $q = null) {
-		return $this->getArray($this->addSlugQuery($slug, $q));
-	}
-  
-	public function countBySlug($slug, Doctrine_Query $q = null) {
-		return $this->count($this->addSlugQuery($slug, $q));
-	}
+  // -----------------------------------------
+  // DRY
+  // -----------------------------------------
 
-	public function getOneBySlug($slug, Doctrine_Query $q = null) {
-		return $this->getOne($this->addSlugQuery($slug, $q));
-	}
+  public function getArrayActiveBySlug($slug, Doctrine_Query $q = null)
+  {
+    return $this->getArrayActive($this->addSlugQuery($slug, $q));
+  }
 
-	public function getBySlug($slug, Doctrine_Query $q = null) {
-		return $this->get($this->addSlugQuery($slug, $q));
-	}
-	
-	// -----------------------------------------
-		
-	public function getArrayActiveByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null) {
-		return $this->getArrayActive($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
-	}
-  
-	public function countActiveByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null) {
-		return $this->countActive($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
-	}
+  public function countActiveBySlug($slug, Doctrine_Query $q = null)
+  {
+    return $this->countActive($this->addSlugQuery($slug, $q));
+  }
 
-	public function getOneActiveByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null) {
-		return $this->getOneActive($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
-	}
+  public function getOneActiveBySlug($slug, Doctrine_Query $q = null)
+  {
+    return $this->getOneActive($this->addSlugQuery($slug, $q));
+  }
 
-	public function getActiveByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null) {
-		return $this->getActive($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
-	}
-	
-	public function getArrayByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null) {
-		return $this->getArray($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
-	}
-  
-	public function countByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null) {
-		return $this->count($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
-	}
+  public function getActiveBySlug($slug, Doctrine_Query $q = null)
+  {
+    return $this->getActive($this->addSlugQuery($slug, $q));
+  }
 
-	public function getOneByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null) {
-		return $this->getOne($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
-	}
+  public function getArrayBySlug($slug, Doctrine_Query $q = null)
+  {
+    return $this->getArray($this->addSlugQuery($slug, $q));
+  }
 
-	public function getByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null) {
-		return $this->get($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
-	}
-	
-	// -----------------------------------------
-	
-	public function getArrayActive(Doctrine_Query $q = null) {
-		return $this->getArray($this->addActiveQuery($q));
-	}
-  
-	public function countActive(Doctrine_Query $q = null) {
-		return $this->count($this->addActiveQuery($q));
-	}
+  public function countBySlug($slug, Doctrine_Query $q = null)
+  {
+    return $this->count($this->addSlugQuery($slug, $q));
+  }
 
-	public function getOneActive(Doctrine_Query $q = null) {
-		return $this->getOne($this->addActiveQuery($q));
-	}
+  public function getOneBySlug($slug, Doctrine_Query $q = null)
+  {
+    return $this->getOne($this->addSlugQuery($slug, $q));
+  }
 
-	public function getActive(Doctrine_Query $q = null) {
-		return $this->get($this->addActiveQuery($q));
-	}
+  public function getBySlug($slug, Doctrine_Query $q = null)
+  {
+    return $this->get($this->addSlugQuery($slug, $q));
+  }
 
-	// -----------------------------------------
-	
-	public function getArrayInactive(Doctrine_Query $q = null) {
-		return $this->getArray($this->addInactiveQuery($q));
-	}
+  // -----------------------------------------
 
-	public function countInactive(Doctrine_Query $q = null) {
-		return $this->count($this->addInactiveQuery($q));
-	}
+  public function getArrayActiveByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null)
+  {
+    return $this->getArrayActive($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
+  }
 
-	public function getOneInactive(Doctrine_Query $q = null) {
-		return $this->getOne($this->addInactiveQuery($q));
-	}
+  public function countActiveByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null)
+  {
+    return $this->countActive($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
+  }
 
-	public function getInactive(Doctrine_Query $q = null) {
-		return $this->get($this->addInactiveQuery($q));
-	}
-	
-	// -----------------------------------------
-	
-	public function getArray(Doctrine_Query $q = null) {
-		return $this->addQuery($q)->fetchArray();
-	}
-  
-	public function count(Doctrine_Query $q = null) {
-		return $this->addQuery($q)->count();
-	}
+  public function getOneActiveByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null)
+  {
+    return $this->getOneActive($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
+  }
 
-	public function getOne(Doctrine_Query $q = null) {
-		return $this->addQuery($q)->fetchOne();
-	}
+  public function getActiveByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null)
+  {
+    return $this->getActive($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
+  }
 
-	public function get(Doctrine_Query $q = null) {
-		return $this->addQuery($q)->execute();
-	}
-	
-	// -----------------------------------------
-	/* Return Query */
-	// -----------------------------------------
-    
-	public function addSlugQuery($slug, Doctrine_Query $q = null) {
-		return $this->addQuery($q)
-			->innerJoin('p.Translation pt')
-			->where('pt.slug = ?', $slug);
-	}
-	
-	public function addLangQuery($lang, Doctrine_Query $q = null) {
-		return $this->addQuery($q)
-			->innerJoin('p.Translation pt')
-			->where('pt.lang = ?', $lang);
-	}
-  
-	public function addActiveQuery(Doctrine_Query $q = null) {
-		return $this->addQuery($q)->addWhere('p.is_active = ?', 1);
-	}
+  public function getArrayByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null)
+  {
+    return $this->getArray($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
+  }
 
-	public function addInactiveQuery(Doctrine_Query $q = null) {
-		return $this->addQuery($q)->addWhere('p.is_active = ?', 0);
-	}
-    
-	public function addQuery(Doctrine_Query $q = null) {
-		if (is_null($q)) {$q = $this->createQuery('p');}
-		$alias = $q->getRootAlias();
-		$q->addOrderBy($alias . '.created_at DESC');
-		return $q;
-	}
-	
-	// -----------------------------------------
-	/* default */
-	// -----------------------------------------
+  public function countByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null)
+  {
+    return $this->count($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
+  }
 
-	public static function getInstance() {
-		return Doctrine_Core::getTable('programme');
-	}
+  public function getOneByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null)
+  {
+    return $this->getOne($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
+  }
+
+  public function getByLang($lang, $limit = 10, $offset = 0, Doctrine_Query $q = null)
+  {
+    return $this->get($this->addLangQuery($lang, $q)->limit($limit)->offset($offset));
+  }
+
+  // -----------------------------------------
+
+  public function getArrayActive(Doctrine_Query $q = null)
+  {
+    return $this->getArray($this->addActiveQuery($q));
+  }
+
+  public function countActive(Doctrine_Query $q = null)
+  {
+    return $this->count($this->addActiveQuery($q));
+  }
+
+  public function getOneActive(Doctrine_Query $q = null)
+  {
+    return $this->getOne($this->addActiveQuery($q));
+  }
+
+  public function getActive(Doctrine_Query $q = null)
+  {
+    return $this->get($this->addActiveQuery($q));
+  }
+
+  // -----------------------------------------
+
+  public function getArrayInactive(Doctrine_Query $q = null)
+  {
+    return $this->getArray($this->addInactiveQuery($q));
+  }
+
+  public function countInactive(Doctrine_Query $q = null)
+  {
+    return $this->count($this->addInactiveQuery($q));
+  }
+
+  public function getOneInactive(Doctrine_Query $q = null)
+  {
+    return $this->getOne($this->addInactiveQuery($q));
+  }
+
+  public function getInactive(Doctrine_Query $q = null)
+  {
+    return $this->get($this->addInactiveQuery($q));
+  }
+
+  // -----------------------------------------
+
+  public function getArray(Doctrine_Query $q = null)
+  {
+    return $this->addQuery($q)->fetchArray();
+  }
+
+  public function count(Doctrine_Query $q = null)
+  {
+    return $this->addQuery($q)->count();
+  }
+
+  public function getOne(Doctrine_Query $q = null)
+  {
+    return $this->addQuery($q)->fetchOne();
+  }
+
+  public function get(Doctrine_Query $q = null)
+  {
+    return $this->addQuery($q)->execute();
+  }
+
+  // -----------------------------------------
+  /* Return Query */
+  // -----------------------------------------
+
+  public function addSlugQuery($slug, Doctrine_Query $q = null)
+  {
+    return $this->addQuery($q)
+        ->innerJoin('p.Translation pt')
+        ->where('pt.slug = ?', $slug);
+  }
+
+  public function addLangQuery($lang, Doctrine_Query $q = null)
+  {
+    return $this->addQuery($q)
+        ->innerJoin('p.Translation pt')
+        ->where('pt.lang = ?', $lang);
+  }
+
+  public function addActiveQuery(Doctrine_Query $q = null)
+  {
+    return $this->addQuery($q)->addWhere('p.is_active = ?', 1);
+  }
+
+  public function addInactiveQuery(Doctrine_Query $q = null)
+  {
+    return $this->addQuery($q)->addWhere('p.is_active = ?', 0);
+  }
+
+  public function addQuery(Doctrine_Query $q = null)
+  {
+    if (is_null($q)) {
+      $q     = $this->createQuery('p');
+    }
+    $alias = $q->getRootAlias();
+    $q->addOrderBy($alias . '.created_at DESC');
+    return $q;
+  }
+
+  // -----------------------------------------
+  /* default */
+  // -----------------------------------------
+
+  public static function getInstance()
+  {
+    return Doctrine_Core::getTable('programme');
+  }
 
 }

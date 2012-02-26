3f0452e15d1e19b051f78266b598ed77909904dc
Clement Gautier <clement.gautier.76@gmail.com>
2012-02-26 15:30:51 +0100

    Styled the showTrees action on programs

--- apps/backend/modules/programme/templates/showTreesSuccess.php
+++ apps/backend/modules/programme/templates/showTreesSuccess.php
@@ -1,7 +1,9 @@
+<div id="sf_admin_container">
+
 <fieldset>
   <legend><?php echo __("General") ?></legend>
   <h3><?php echo __("%number% trees planted", array('%number%' => $programme->countTrees())) ?></h3>
-  <ul>
+  <ul style="padding:10px; margin-left:25px;">
     <li><?php echo __("%number% trees planted using partner voucher", array('%number%' => $programme->countTreesVoucherPartner())) ?></li>
     <li><?php echo __("%number% trees planted using user voucher", array('%number%' => $programme->countTreesVoucherUser())) ?></li>
     <li><?php echo __("%number% trees planted using user account", array('%number%' => $programme->countTreesUser())) ?></li>
@@ -20,15 +22,33 @@
       </tr>
     </thead>
     <tbody>
+      <?php $totalPartnerVoucherCount = 0; ?>
+      <?php $totalUserVoucherCount = 0; ?>
+      <?php $totalUserAccountCount = 0; ?>
+      
       <?php foreach ($months as $key => $values) : ?>
-      <tr>
-        <td><?php echo $key ?></td>
-        <td><?php echo ($partnerVoucherCount = isset($values['partnerVoucherCount']) ? $values['partnerVoucherCount'] : 0) ?></td>
-        <td><?php echo ($userVoucherCount = isset($values['userVoucherCount']) ? $values['userVoucherCount'] : 0) ?></td>
-        <td><?php echo ($userAccountCount =  isset($values['userAccountCount']) ? $values['userAccountCount'] : 0) ?></td>
-        <td><?php echo $partnerVoucherCount + $userVoucherCount + $userAccountCount ?></td>
-      </tr>
+        <tr>
+          <td><?php echo $key ?></td>
+          <td><?php echo ($partnerVoucherCount = isset($values['partnerVoucherCount']) ? $values['partnerVoucherCount'] : 0) ?></td>
+          <td><?php echo ($userVoucherCount = isset($values['userVoucherCount']) ? $values['userVoucherCount'] : 0) ?></td>
+          <td><?php echo ($userAccountCount =  isset($values['userAccountCount']) ? $values['userAccountCount'] : 0) ?></td>
+          <td><?php echo $partnerVoucherCount + $userVoucherCount + $userAccountCount ?></td>
+        </tr>
+        <?php $totalPartnerVoucherCount += $partnerVoucherCount; ?>
+        <?php $totalUserVoucherCount += $userVoucherCount; ?>
+        <?php $totalUserAccountCount += $userAccountCount; ?>
       <?php endforeach ?>
     </tbody>
+    <tfoot>
+      <tr>
+        <th><?php echo __("Total:") ?></th>
+        <th><?php echo $totalPartnerVoucherCount ?></th>
+        <th><?php echo $totalUserVoucherCount ?></th>
+        <th><?php echo $totalUserAccountCount ?></th>
+        <th><?php echo $totalPartnerVoucherCount + $totalUserVoucherCount + $totalUserAccountCount ?></th>
+      </tr>
+    </tfoot>
   </table>
 </fieldset>
+
+</div>

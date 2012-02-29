f9ae30b343548b3123b7719c7f6d972ebc013d0e
Clement Gautier <clement.gautier.76@gmail.com>
2012-02-29 02:18:26 +0100

    Code review

--- apps/frontend/modules/blog/actions/actions.class.php
+++ apps/frontend/modules/blog/actions/actions.class.php
@@ -8,56 +8,63 @@
  * @author     Olivier Dolbeau
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class blogActions extends sfActions {
- /**
-  * Executes index action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeIndex(sfWebRequest $request) {
+class blogActions extends sfActions
+{
 
+  /**
+   * Executes index action
+   *
+   * @param sfRequest $request A request object
+   */
+  public function executeIndex(sfWebRequest $request)
+  {
     // S'il s'agit d'une requête AJAX, on redirige vers la bonne action (en fonction de l'élément à récupérer)
-    if($request->isXmlHttpRequest()) {
-      $this->forward('blog', 'AjaxRetrieve'.ucfirst($request->getParameter('changement')));
+    if ($request->isXmlHttpRequest()) {
+      $this->forward('blog', 'AjaxRetrieve' . ucfirst($request->getParameter('changement')));
     }
-    
   }
 
-  public function executeAjaxRetrieveArticles(sfWebRequest $request) {
+  public function executeAjaxRetrieveArticles()
+  {
     $this->renderComponent('blog', 'articlesBloc');
     return sfView::NONE;
   }
 
-  public function executeAjaxRetrieveProgrammes(sfWebRequest $request) {
+  public function executeAjaxRetrieveProgrammes()
+  {
     $this->renderComponent('blog', 'programmesBloc');
     return sfView::NONE;
   }
 
-  public function executeAjaxRetrievePartenaires(sfWebRequest $request) {
+  public function executeAjaxRetrievePartenaires()
+  {
     $this->renderComponent('blog', 'partenairesBloc');
     return sfView::NONE;
   }
 
- /**
-  * Executes viewArticle action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeViewElement(sfWebRequest $request) {
+  /**
+   * Executes viewArticle action
+   *
+   * @param sfRequest $request A request object
+   */
+  public function executeViewElement(sfWebRequest $request)
+  {
     $this->type = $request->getParameter('type');
     $this->element = Doctrine::getTable($this->type)->getOneBySlug($request->getParameter("slug"));
   }
 
- /**
-  * Executes viewList action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeViewList(sfWebRequest $request) {
-  	$this->type = $request->getParameter('type');
-  	if($this->type == 'organisme')
-    	$this->elements = Doctrine::getTable($this->type)->getByLang($this->getUser()->getCulture());
+  /**
+   * Executes viewList action
+   *
+   * @param sfRequest $request A request object
+   */
+  public function executeViewList(sfWebRequest $request)
+  {
+    $this->type = $request->getParameter('type');
+    if ($this->type == 'organisme')
+      $this->elements = Doctrine::getTable($this->type)->getByLang($this->getUser()->getCulture());
     else
-    	$this->elements = Doctrine::getTable($this->type)->getActiveByLang($this->getUser()->getCulture());
+      $this->elements = Doctrine::getTable($this->type)->getActiveByLang($this->getUser()->getCulture());
   }
+
 }
